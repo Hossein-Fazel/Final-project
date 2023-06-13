2f24035c7f4614d0762274d673ede90e68c3f235
Hossein <fazel.hosein9654@gmail.com>
2023-06-13 18:43:04 +0330

    add twitterak

--- include/Display_Class.hpp
+++ include/Display_Class.hpp
@@ -1,11 +1,14 @@
 #ifndef DISPLAY_HPP
 #define DISPLAY_HPP
 
+#include <string>
+
 class display
 {
     public:
         void help();
-        void menu();
+        // void menu();
+        void login(std::string , std::string);
         void login();
         void signup();
         void show_tweet();
