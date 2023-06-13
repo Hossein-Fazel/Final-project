a22ec4adffc511ccaad23719bf1c7ad8b1da7dff
Hassan <Hs.soleymani7@gmail.com>
2023-06-13 07:35:47 +0330

    adding the Get_Functions to the User_Class

---
+++ src/User_Class.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "User_Class.hpp"
+
+//===================================================  Get_Functions =================================================== 
+
+std::string user::get_name() const                      // returning the name of the user
+{
+    return name;
+}
+
+
+std::string user::get_biography() const                // returning the biography of the user
+{
+    return biography[160];
+}
+
+
+std::string user::get_link() const                    // returning the link of the user
+{
+    return link;
+}
+
+
+std::string user::get_birthday() const              // returning the date of birth of the user
+{
+    return birthday;
+}
+
+
+std::string user::get_phone() const                // returning the phone number of the user
+{
+    return phone_number;
+}
+
+
+std::string user::get_password() const            // returning the password of the user's account
+{
+    return password;
+}
+
+
+std::string user::get_header() const            // returning the color of the header that user had chosen 
+{
+    return header;
+}
+
+//===================================================  Get_Functions =================================================== 
+
